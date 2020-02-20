462f1bd7b19e6d22d88771bb350d146037d8a372
psingh <prabhsimran.singh@savoirfairelinux.com>
2020-02-20 15:46:47 -0500

    My Portfolio project

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,53 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "animate.css/animate.min.css";
+import ScrollAnimation from "react-animate-on-scroll";
 
 class About extends Component {
   render() {
-
-    if(this.props.data){
+    if (this.props.data) {
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profilepic = "images/" + this.props.data.image;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
+      var phone = this.props.data.phone;
       var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+      // var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
+        <div className="row">
+          <div className="three columns">
+            <ScrollAnimation animateIn="bounceInLeft" duration="2">
+              <img
+                className="profile-pic"
+                src={profilepic}
+                alt="Prabhsimran Singh's Profile Pic"
+              />
+            </ScrollAnimation>
+          </div>
+          <div className="nine columns main-col">
+            <ScrollAnimation animateIn="bounceInLeft" duration="2">
+              <h2>About Me</h2>
+              <p>{bio}</p>
+            </ScrollAnimation>
             <div className="row">
-               <div className="columns contact-details">
+              <div className="columns contact-details">
+                <ScrollAnimation animateIn="bounceInRight" duration="2">
                   <h2>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
-               </div>
-               <div className="columns download">
+                    <span>{name}</span>
+                    <br />
+                    <span>
+                      {street}
+                      <br />
+                      {city} {state}, {zip}
+                    </span>
+                    <br />
+                    <span>{phone}</span>
+                    <br />
+                    <span>{email}</span>
+                  </p>
+                </ScrollAnimation>
+              </div>
+              {/*<div className="columns download">
                   <p>
                      <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
                   </p>
-               </div>
+               </div> */}
             </div>
-         </div>
-      </div>
-
-   </section>
+          </div>
+        </div>
+      </section>
     );
   }
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,116 +1,179 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import * as emailjs from "emailjs-com";
 
 class Contact extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      name: "",
+      email: "",
+      subject: "",
+      sent: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.sendEmail = this.sendEmail.bind(this);
+    // this.resetForm = this.resetForm.bind();
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    console.log(this.state.name + this.state.email + this.state.message);
+  }
 
-    if(this.props.data){
+  sendEmail(event) {
+    event.preventDefault();
+    const templateId = "template_o1PLQLb1";
+    let templateParams = {
+      message_html: this.state.message,
+      from_name: this.state.name,
+      from_email: this.state.email,
+      to_name: "Prabhsimran",
+      subject: this.state.subject,
+      to_email: "<prabhsimran.06@gmail.com>"
+    };
+    emailjs
+      .send("gmail", templateId, templateParams, "user_fxj1tNRdoOM7Wuw1l5W12")
+      .then(
+        result => {
+          console.log(result.text);
+          if (result.text === "OK") {
+            this.setState({
+              sent: true,
+              message: "",
+              name: "",
+              email: "",
+              subject: ""
+            });
+            console.log(this.state.message + " 11");
+          }
+        },
+        error => {
+          console.log(error.text);
+        }
+      );
+  }
+  render() {
+    if (this.props.data) {
       var name = this.props.data.name;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
       var email = this.props.data.email;
       var message = this.props.data.contactmessage;
     }
 
     return (
       <section id="contact">
+        <div className="row section-head">
+          <div className="two columns header-col">
+            <h1>
+              <span>Get In Touch.</span>
+            </h1>
+          </div>
 
-         <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">{message}</p>
+          </div>
+        </div>
 
-            <div className="two columns header-col">
+        <div className="row">
+          <div className="eight columns">
+            <form
+              action=""
+              id="contact-Form"
+              name="contactForm"
+              onSubmit={this.sendEmail}
+            >
+              <fieldset>
+                <div>
+                  <label htmlFor="contactName">
+                    Name <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactName"
+                    name="name"
+                    onChange={this.handleChange}
+                  />
+                </div>
 
-               <h1><span>Get In Touch.</span></h1>
+                <div>
+                  <label htmlFor="contactEmail">
+                    Email <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactEmail"
+                    name="email"
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="contactSubject">Subject</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactSubject"
+                    name="subject"
+                    onChange={this.handleChange}
+                  />
+                </div>
 
+                <div>
+                  <label>
+                    Message <span className="required">*</span>
+                  </label>
+                  <textarea
+                    cols="50"
+                    rows="5"
+                    id="contactMessage"
+                    name="message"
+                    onChange={this.handleChange}
+                  ></textarea>
+                </div>
+
+                <div>
+                  <button className="submit">Submit</button>
+                </div>
+              </fieldset>
+            </form>
+
+            <div>
+              {!this.state.sent && (
+                <div className="feedbackmessage">
+                  <i className="fa fa-check"></i> Your message was sent, Thank
+                  you!
+                  <br />
+                </div>
+              )}
             </div>
 
-            <div className="ten columns">
+            <div id="message-warning"> Error boy</div>
+          </div>
 
-                  <p className="lead">{message}</p>
-
+          <aside className="four columns footer-widgets">
+            <div className="widget widget_contact">
+              <h4>Address and Phone</h4>
+              <p className="address">
+                {name}
+                <br />
+                {street} <br />
+                {city}, {state}, {zip}
+                <br />
+                <span>{email}</span>
+              </p>
             </div>
-
-         </div>
-
-         <div className="row">
-            <div className="eight columns">
-
-               <form action="" method="post" id="contactForm" name="contactForm">
-					<fieldset>
-
-                  <div>
-						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange}/>
-                  </div>
-
-                  <div>
-                     <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                  </div>
-
-                  <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
-                  </div>
-					</fieldset>
-				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-                  <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
-           </div>
-
-
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
-
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
-
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
-            </aside>
-      </div>
-   </section>
+          </aside>
+        </div>
+      </section>
     );
   }
 }
